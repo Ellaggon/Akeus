6d5083e6d4dc20cbd336f19f6d12145e5d1a5856
Ellioth Aguirre <ellaggon@gmail.com>
2024-06-19 22:52:12 -0400

    add logo akeus on nav

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -2,79 +2,44 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-import { useState } from "react";
 
 export const Header = () => {
   const { data: session } = useSession();
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <header>
       <nav className="flex justify-between p-3 items-center">
-        <Link href="/" className="text-xl">
-          AKEUS
-        </Link>
-        <ul
-          className={`flex-col md:flex md:flex-row md:gap-5 md:text-xl fixed left-[40%] justify-center ${
-            menuOpen ? "flex" : "hidden"
-          } md:flex`}
-        >
-          <Link href="/">
-            <div className="inline-flex flew-row justify-center items-center text-white text-center font-medium bg-opacity-60 opacity-70 hover:opacity-100 hover:bg-slate-900 rounded-lg cursor-pointer px-3 py-2.5 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 dark:focus:ring-[#1da1f2]/55 hover:shadow-lg hover:scale-90 transition">
-              <p>Inicio</p>
-            </div>
-          </Link>
-          <Link href="/store">
-            <div className="inline-flex flew-row justify-center items-center text-white text-center font-medium bg-opacity-60 opacity-70 hover:opacity-100 hover:bg-slate-700 rounded-lg cursor-pointer px-3 py-2.5 z-20 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 dark:focus:ring-[#1da1f2]/55 hover:shadow-lg hover:scale-90 transition">
-              <p>Productos</p>
-            </div>
-          </Link>
-          <Link href="/contact">
-            <div className="inline-flex flew-row justify-center items-center text-white text-center font-medium bg-opacity-60 opacity-70 hover:opacity-100 hover:bg-slate-700 rounded-lg cursor-pointer px-3 py-2.5 z-20 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 dark:focus:ring-[#1da1f2]/55 hover:shadow-lg hover:scale-90 transition">
-              <p>Contacto</p>
-            </div>
-          </Link>
-          {session && (
-            <div>
-              <button className="md:hidden" onClick={() => signOut()}>
-                LogOut
+        <div>
+          {session ? (
+            <button
+              className="px-3 border-b-2 text-stone-500 border-black hover:border-stone-500 justify-end items-center"
+              onClick={() => signOut()}
+            >
+              LogOut
+            </button>
+          ) : (
+            <div className="flex justify-center">
+              <button
+                className="rounded-2xl border px-5 py-1 hover:bg-slate-900"
+                onClick={() => signIn()}
+              >
+                Login con Google
               </button>
             </div>
           )}
-        </ul>
-        <div className="flex items-center cursor-pointer">
-          <Image
-            className="w-10 mx-3"
+        </div>
+        <Link href="/" className="text-xl w-20">
+          <img src="/images/favicon-sbg.webp" alt="img" style={{filter: "drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))"}}/>
+        </Link>
+        <div className={`flex cursor-pointer text-sm h-10 items-center ${session ? "flex" : "hidden"}`}>
+          {
+            session?.user?.image? <img
+            className="w-10 h-10 mx-3"
             src={session?.user?.image || ""}
             alt={session?.user?.name || ""}
-          />
-          <p className="mr-5">{`${session?.user?.name || ""}`}</p>
-          <div>
-            {session ? (
-              <div>
-                <button
-                  className="px-3 border-b-2 border-black hover:border-white justify-end hidden md:flex"
-                  onClick={() => signOut()}
-                >
-                  LogOut
-                </button>
-              </div>
-            ) : (
-              <div className="flex justify-center">
-                <button
-                  className="rounded-2xl border px-5 py-1 hover:bg-slate-900"
-                  onClick={() => signIn()}
-                >
-                  Login con Google
-                </button>
-              </div>
-            )}
-          </div>
-          <button className="md:hidden text-xl" onClick={toggleMenu}>
-            &#9776;
-          </button>
+          ></img> : ""
+          }
+          <p className="mr-5 text-stone-500">{`${session?.user?.name || ""}`}</p>
         </div>
       </nav>
     </header>
