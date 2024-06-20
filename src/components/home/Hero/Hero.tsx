d8c70f69b3b7f6366ce0b4d9fc8027600dc51063
Ellioth Aguirre <ellaggon@gmail.com>
2024-06-19 22:51:01 -0400

    edith hero & descriptions

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,10 +1,10 @@
 export const Hero = () => {
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center mt-10">
       <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-black to-yellow-700 text-7xl mt-12 mb-4 tracking-wide text-center">
         Akeus
       </h1>
-      <h2 className="text-2xl mb-12 text-center">
+      <h2 className="sm:text-2xl mb-12 text-center">
         Empowering Your Tomorrow, Today!
       </h2>
     </section>
